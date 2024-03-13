08da9cc7a2bc99a37424583347348e22461d2eeb
Jokir <doner24072004@gmail.com>
2024-03-13 21:08:51 +0200

    Added VillaNumber

---
+++ MagicVilla_VillaAPI/Models/Dto/VillaNumberDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class VillaNumberDTO
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
