c9dd48f0a744b25f8405379d40c78645f149a32d
Jokir <doner24072004@gmail.com>
2024-04-11 22:42:11 +0300

    Login issue were fixed.

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -63,7 +63,7 @@ namespace MagicVilla_VillaAPI.Repository
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.UserName.ToString()),
                     new Claim(ClaimTypes.Role, roles.FirstOrDefault())
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
@@ -76,7 +76,6 @@ namespace MagicVilla_VillaAPI.Repository
             {
                 Token = tokenHandler.WriteToken(token),
                 User = _mapper.Map<UserDTO>(user),
-                Role = roles.FirstOrDefault(),
             };
 
             return loginResponseDTO;
--- MagicVilla_Web/Controllers/AuthController.cs
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace MagicVilla_Web.Controllers
@@ -31,22 +32,25 @@ namespace MagicVilla_Web.Controllers
         public async Task<IActionResult> Login(LoginRequestDTO obj)
         {
             APIResponse response = await _authService.LoginAsync<APIResponse>(obj);
-            if(response != null && response.IsSuccess)
+            if (response != null && response.IsSuccess)
             {
                 LoginResponseDTO model = JsonConvert.DeserializeObject<LoginResponseDTO>(Convert.ToString(response.Result));
 
+                var handler = new JwtSecurityTokenHandler();
+                var jwt = handler.ReadJwtToken(model.Token);
+
                 var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
-                identity.AddClaim(new Claim(ClaimTypes.Name,model.User.UserName));
-                identity.AddClaim(new Claim(ClaimTypes.Role,model.User.Role));
+                identity.AddClaim(new Claim(ClaimTypes.Name, jwt.Claims.FirstOrDefault(u => u.Type == "unique_name").Value)); //must be "unique_name"
+                identity.AddClaim(new Claim(ClaimTypes.Role, jwt.Claims.FirstOrDefault(u => u.Type == "role").Value));
                 var principal = new ClaimsPrincipal(identity);
                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
 
                 HttpContext.Session.SetString(SD.SessionToken, model.Token);
-                return RedirectToAction("Index","Home");
+                return RedirectToAction("Index", "Home");
             }
             else
             {
-                ModelState.AddModelError("CustomError",response.ErrorMessages.FirstOrDefault());
+                ModelState.AddModelError("CustomError", response.ErrorMessages.FirstOrDefault());
                 return View(obj);
             }
         }
@@ -63,7 +67,7 @@ namespace MagicVilla_Web.Controllers
         public async Task<IActionResult> Register(RegisterationRequestDTO obj)
         {
             APIResponse result = await _authService.RegisterAsync<APIResponse>(obj);
-            if(result != null && result.IsSuccess)
+            if (result != null && result.IsSuccess)
             {
                 return RedirectToAction("Login");
             }
@@ -74,7 +78,7 @@ namespace MagicVilla_Web.Controllers
         {
             await HttpContext.SignOutAsync();
             HttpContext.Session.SetString(SD.SessionToken, "");
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult AccessDenied()
